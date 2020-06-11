18bcc954e0ebb7454503386dfe3caa04b63e8f45
TanujRapaka <66767100+TanujRapaka@users.noreply.github.com>
2020-06-11 19:46:57 +0530

    Add files via upload
    working vm deployement powershell script

---
+++ Dedployement script.ps1
@@ -0,0 +1,14 @@
+﻿
+New-AzResourceGroup -Name "Wu-demo4-group" -Location "EastUS"
+
+$username = "admin"
+$password = ConvertTo-SecureString "Password@123" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ("azureuser", $password)
+
+New-AzVM -Name LinuxVm1 -Credential `
+$cred -ResourceGroupName Wu-demo3-group `
+-Image Canonical:UbuntuServer:18.04-LTS:latest ` -Size Standard_D2S_V3
+
+New-AzVM -Name LinuxVm2 -Credential `
+$cred -ResourceGroupName Wu-demo3-group `
+-Image Canonical:UbuntuServer:18.04-LTS:latest ` -Size Standard_D2S_V3 
